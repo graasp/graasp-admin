c069f79005da37b339238b5cdc5e8766ee8e5a3e
HadiKhai <35467011+HadiKhai@users.noreply.github.com>
2021-07-15 11:54:38 +0200

    refactor: return Loader

--- src/components/items/ItemScreen.js
+++ src/components/items/ItemScreen.js
@@ -60,127 +60,116 @@ const ItemScreen = () => {
 
   return (
     <div>
-      {!isLoading && (
-        <>
-          <Box
-            justifyContent="left"
-            display="flex"
-            p={1}
-            bgcolor="background.paper"
-          >
-            <Box display="flex" flexDirection="row">
-              <Box p={2}>
-                <ItemIcon
-                  size="100"
-                  type={item.get('type')}
-                  extra={item.get('extra')}
-                  name={item.get('name')}
-                />
-              </Box>
-              <Box display="flex" p={2} flexDirection="column">
-                <Typography align="left">{`Id: ${item.get('id')}`}</Typography>
-                <Typography align="left">
-                  {`Type: ${item.get('type')}`}
-                </Typography>
-                <Typography align="left">
-                  {`Name: ${item.get('name')}`}
-                </Typography>
-                <Typography align="left">
-                  {`Description: ${item.get('description')}`}
-                </Typography>
-              </Box>
-              <Box display="flex" p={2} flexDirection="column">
-                <Typography align="left">
-                  {`Owner `}
-                  <Link
-                    color="inherit"
-                    to={buildMemberPath(item.get('creator'))}
-                  >
-                    {item.get('ownerName')}
-                  </Link>
-                </Typography>
-                <Typography align="left">
-                  {`Created At: ${formatDate(item.get('createdAt'))}`}
-                </Typography>
-                <Typography
-                  align="left"
-                  id={buildNavigationLink(item.get('id'))}
-                >
-                  {`Last time updated: ${formatDate(item.get('updatedAt'))}`}
-                </Typography>
+      <>
+        <Box
+          justifyContent="left"
+          display="flex"
+          p={1}
+          bgcolor="background.paper"
+        >
+          <Box display="flex" flexDirection="row">
+            <Box p={2}>
+              <ItemIcon
+                size="100"
+                type={item.get('type')}
+                extra={item.get('extra')}
+                name={item.get('name')}
+              />
+            </Box>
+            <Box display="flex" p={2} flexDirection="column">
+              <Typography align="left">{`Id: ${item.get('id')}`}</Typography>
+              <Typography align="left">
+                {`Type: ${item.get('type')}`}
+              </Typography>
+              <Typography align="left">
+                {`Name: ${item.get('name')}`}
+              </Typography>
+              <Typography align="left">
+                {`Description: ${item.get('description')}`}
+              </Typography>
+            </Box>
+            <Box display="flex" p={2} flexDirection="column">
+              <Typography align="left">
+                {`Owner `}
+                <Link color="inherit" to={buildMemberPath(item.get('creator'))}>
+                  {item.get('ownerName')}
+                </Link>
+              </Typography>
+              <Typography align="left">
+                {`Created At: ${formatDate(item.get('createdAt'))}`}
+              </Typography>
+              <Typography align="left" id={buildNavigationLink(item.get('id'))}>
+                {`Last time updated: ${formatDate(item.get('updatedAt'))}`}
+              </Typography>
 
-                <Typography
-                  align="left"
-                  id={buildNavigationLink(item.get('id'))}
-                >
-                  {`Parents: `}
-                </Typography>
-                <Breadcrumbs maxItems={2} aria-label="breadcrumb">
-                  <Link color="inherit" to={ITEMS_PATH}>
-                    All Items
+              <Typography align="left" id={buildNavigationLink(item.get('id'))}>
+                {`Parents: `}
+              </Typography>
+              <Breadcrumbs maxItems={2} aria-label="breadcrumb">
+                <Link color="inherit" to={ITEMS_PATH}>
+                  All Items
+                </Link>
+                {parents.map(({ name, id }) => (
+                  <Link color="inherit" to={buildItemPath(id)}>
+                    {name}
                   </Link>
-                  {parents.map(({ name, id }) => (
-                    <Link color="inherit" to={buildItemPath(id)}>
-                      {name}
-                    </Link>
-                  ))}
-                </Breadcrumbs>
-              </Box>
+                ))}
+              </Breadcrumbs>
             </Box>
           </Box>
-          <div className={classes.root}>
-            <AppBar position="static" color="default">
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                variant="fullWidth"
-                scrollButtons="off"
-                textColor="primary"
-                aria-label="scrollable-prevent-tabs"
-                indicatorColor="primary"
-              >
-                <Tab
-                  label="Children"
-                  id={`scrollable-prevent-tab-${0}`}
-                  aria-controls={`scrollable-prevent-tabpanel-${0}`}
-                />
-                <Tab
-                  label="Members"
-                  id={`scrollable-prevent-tab-${1}`}
-                  aria-controls={`scrollable-prevent-tabpanel-${1}`}
-                />
-                <Tab
-                  label="Settings"
-                  id={`scrollable-prevent-tab-${2}`}
-                  aria-controls={`scrollable-prevent-tabpanel-${2}`}
-                />
-              </Tabs>
-            </AppBar>
-            <TabPanel value={value} index={0}>
-              {children.isEmpty() ? (
-                <Typography>No Children found</Typography>
-              ) : (
-                <ItemsTable
-                  empty={false}
-                  items={children}
-                  id={buildChildrenItemsTableId(itemId)}
-                />
-              )}
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              <MembersTable
-                empty={false}
-                members={members}
-                id={buildMembersTableId(itemId)}
+        </Box>
+        <div className={classes.root}>
+          <AppBar position="static" color="default">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              variant="fullWidth"
+              scrollButtons="off"
+              textColor="primary"
+              aria-label="scrollable-prevent-tabs"
+              indicatorColor="primary"
+            >
+              <Tab
+                label="Children"
+                id={`scrollable-prevent-tab-${0}`}
+                aria-controls={`scrollable-prevent-tabpanel-${0}`}
               />
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              <Typography>Extra:</Typography>
-              <ReactJson src={item.get('extra')} />
-            </TabPanel>
-          </div>
-        </>
-      )}
+              <Tab
+                label="Members"
+                id={`scrollable-prevent-tab-${1}`}
+                aria-controls={`scrollable-prevent-tabpanel-${1}`}
+              />
+              <Tab
+                label="Settings"
+                id={`scrollable-prevent-tab-${2}`}
+                aria-controls={`scrollable-prevent-tabpanel-${2}`}
+              />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0}>
+            {children.isEmpty() ? (
+              <Typography>No Children found</Typography>
+            ) : (
+              <ItemsTable
+                empty={false}
+                items={children}
+                id={buildChildrenItemsTableId(itemId)}
+              />
+            )}
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <MembersTable
+              empty={false}
+              members={members}
+              id={buildMembersTableId(itemId)}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <Typography>Extra:</Typography>
+            <ReactJson src={item.get('extra')} />
+          </TabPanel>
+        </div>
+      </>
     </div>
   );
 };
--- src/components/views/ItemsView.js
+++ src/components/views/ItemsView.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Items from '../items/Items';
 import { hooks } from '../../config/queryClient';
+import Loader from '../common/Loader';
 
 const { useAllItems } = hooks;
 
 const ItemsView = () => {
   const { data: allItems, isLoading } = useAllItems();
 
-  return <>{!isLoading && <Items title="All Items" items={allItems} />}</>;
+  if (isLoading) {
+    return <Loader />;
+  }
+  return <Items title="All Items" items={allItems} />;
 };
 
 export default ItemsView;
