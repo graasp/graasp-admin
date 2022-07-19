66209b771156f38c1bbafa14c50244b72c1e8366
Víctor González <36533965+victorggonzalez@users.noreply.github.com>
2022-07-19 14:49:34 +0200

    fix: include mock constants

--- src/components/views/AdminsView.js
+++ src/components/views/AdminsView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { mockAdmins } from '../../config/constants';
 // import { hooks } from '../../config/queryClient';
 // import Loader from '../common/Loader';
 import Admins from '../admins/Admins';
@@ -9,33 +10,11 @@ import Admins from '../admins/Admins';
 const AdminsView = () => {
   // const { data: allMember, isLoading } = useAllAdmins();
   const { t } = useTranslation();
-
-  const mockAdmins = [
-    {
-      id: '1234',
-      name: 'admin1',
-      roles: 'admin',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'HUN4S@graasp.org',
-      type: 'individual',
-    },
-    {
-      id: '12345',
-      name: 'admin2',
-      roles: 'admin',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'HUN4S@graasp.org',
-      type: 'individual',
-    },
-  ];
+  const admins = mockAdmins;
   // if (isLoading) {
   //   return <Loader />;
   // }
-  return (
-    <Admins title={t('All Admins')} admins={mockAdmins} elementType="admin" />
-  );
+  return <Admins title={t('All Admins')} admins={admins} elementType="admin" />;
 };
 
 export default AdminsView;
--- src/components/views/OrganizationsView.js
+++ src/components/views/OrganizationsView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { mockOrganizations } from '../../config/constants';
 // import { Loader } from '@graasp/ui';
 // import { hooks } from '../../config/queryClient';
 import Organizations from '../organizations/Organizations';
@@ -13,97 +14,8 @@ const OrganizationsView = () => {
   // if (isLoading) {
   //   return <Loader />;
   // }
+  const orgs = mockOrganizations;
 
-  const orgs = [
-    {
-      id: '1234',
-      name: 'GVA',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'gva@graasp.org',
-    },
-    {
-      id: '1235',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1236',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1237',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1234',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1234',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1234',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1234',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1234',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1234',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-    {
-      id: '1234',
-      name: 'LYON',
-      type: 'org',
-      updatedAt: '2022-01-01',
-      createdAt: '2022-01-01',
-      email: 'lyon@graasp.org',
-    },
-  ];
   return (
     <Organizations
       title={t('All Organizations')}
